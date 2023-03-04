f8ccb34089f7b4811cfcf46b4c47952c39a14ac4
amairabhandari <122103269+amairabhandari@users.noreply.github.com>
2023-03-04 17:21:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,39 +1,87 @@
-function add(){
-    background_imgTag= new Image();
-    background_imgTag.onload= uploadBackground;
-    background_imgTag.src= background_image;
+canvas= document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
 
-    rover_imgTag= new Image();
-    rover_imgTag.onload= uploadrover;
-    rover_imgTag.src= rover_image;
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
 }
 
-function uploadBackground(){
-    ctx.drawImage(background_imgTag,0,0,canvas.width, canvas.height);
-}
+function uploadimg() {
 
-function uploadrover(){
-    ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height);
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
 }
 
 window.addEventListener("keydown", my_keydown);
+
 function my_keydown(e)
 {
-    keyPressed =e.keyCode;
-    console.log(keyPressed);
-    if(keyPressed=='38')
-    {
-        up();
-        console.log("up");
-    }
-    if (keyPressed=='40')
-    {
-        down();
-        console.log("down");
-    }
-    if (keyPressed=='37')
-    {
-        right();
-        console.log("right");
-    }
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		{
+			alphabetkey();
+			document.getElementById("d1").innerHTML="You pressed Alphabet Key";
+			console.log("alphabet key");
+		} 
+		else if(keyPressed>=48 && keyPressed<=57)
+		{
+			numberkey();
+        document.getElementById("d1").innerHTML="You pressed Number Key";
+		console.log("number key")
+		}
+		else if(keyPressed=17,keyPressed=18,keyPressed=27)
+		{
+			specialkey();
+			document.getElementById("d1").innerHTML="You pressed Special Key";
+			console.log("special key")
+		}
+		else if(keyPressed>=37 && keyPressed>=40)
+		{
+			arrowkey();
+			document.getElementById("d1").innerHTML="You pressed arrow key";
+			console.log("arrow key")
+		}
+		
+		else{
+			otherkey() ;
+			document.getElementById("d1").innerHTML="You pressed symbol or other key";
+			console.log("other key")
 }
+	}
+
+function alphabetkey()
+{
+	img_image="Alpkey.png";
+	add(); 
+}
+function numberkey()
+{
+	 img_image="numkey.png";
+	 add();
+}
+function arrowkey()
+{
+	img_image="Arrkey.png";
+	add();
+}
+function specialkey()
+{
+	img_image="spkey.png";
+	add();	
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
